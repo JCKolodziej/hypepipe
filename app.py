cef6da5b64a306753ff6b567e3cd03106f7722d0
Kendzioor <wojtekadam1@gmail.com>
2019-07-09 11:26:03 +0200

    Add loading functions for main page and game page

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def load_main_page():
+    return render_template('mainpage.html')
+
+
+@app.route('/game')
+def load_game_page():
+    return render_template('')
 
 
 if __name__ == '__main__':
