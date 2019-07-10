89a2b6155eaac8da50b56d7dd85a905a16f9bc6b
Korneliusz Chodnikiewicz <korneliuszchodnikiewicz@gmail.com>
2019-07-10 13:26:15 +0200

    Add function to check win

--- static/game.js
+++ static/game.js
@@ -1,26 +1,32 @@
-let cell = document.querySelectorAll('[data-position-x="1"][data-position-y="1"]')[0];
+// let cell = document.querySelectorAll('[data-position-x="1"][data-position-y="1"]')[0];
+// let nextcell = document.querySelectorAll('[data-position-x="2"][data-position-y="1"]')[0];
+
+const connectionPairs = {'1':'3', '2':'4', '3':'1', '4':'2'};
+const numberOfTilesToWin = 2;
 
 function findNextCell(currentCell) {
 	let currentCellConnectionEdge = currentCell.dataset['edge-2'];
+	let currentCellPositionX = parseInt(currentCell.dataset.positionX, 10);
+	let currentCellPositionY = parseInt(currentCell.dataset.positionY, 10);
 
 	let nextCellPositionY;
 	let nextCellPositionX;
 
 	if (currentCellConnectionEdge === "1") {
-		nextCellPositionX = parseInt(currentCell.dataset.positionX, 10)
-		nextCellPositionY = parseInt(currentCell.dataset.positionY, 10) - 1;
+		nextCellPositionX = currentCellPositionX;
+		nextCellPositionY = currentCellPositionY - 1;
 
 	} else if (currentCellConnectionEdge === "2") {
-		nextCellPositionX = parseInt(currentCell.dataset.positionX, 10) + 1
-		nextCellPositionY = parseInt(currentCell.dataset.positionY, 10);
+		nextCellPositionX = currentCellPositionX + 1;
+		nextCellPositionY = currentCellPositionY;
 
 	} else if (currentCellConnectionEdge === "3") {
-		nextCellPositionX = parseInt(currentCell.dataset.positionX, 10)
-		nextCellPositionY = parseInt(currentCell.dataset.positionY, 10) + 1;
+		nextCellPositionX = currentCellPositionX;
+		nextCellPositionY = currentCellPositionY + 1;
 
 	} else if (currentCellConnectionEdge === "4") {
-		nextCellPositionX = parseInt(currentCell.dataset.positionX, 10) - 1;
-		nextCellPositionY = parseInt(currentCell.dataset.positionY, 10);
+		nextCellPositionX = currentCellPositionX - 1;
+		nextCellPositionY = currentCellPositionY;
 
 	}
 
@@ -28,4 +34,36 @@ function findNextCell(currentCell) {
 
 }
 
-console.log(findNextCell(cell));
+function isValidConnection(currentCell, nextCell) {
+	let currentCellConnectionEdge = currentCell.dataset['edge-2'];
+	let nextCellConnectionEdge = nextCell.dataset['edge-1'];
+
+	return connectionPairs[currentCellConnectionEdge] === nextCellConnectionEdge
+}
+
+
+function checkWin() {
+	// debugger;
+	let correctTileCount = 0;
+	let currentCell = document.querySelectorAll('[data-position-x="1"][data-position-y="1"]')[0];
+	try {
+		while (correctTileCount < numberOfTilesToWin) {
+			let nextCell = findNextCell(currentCell);
+			if (isValidConnection(currentCell, nextCell)) {
+				currentCell = nextCell;
+				correctTileCount++
+			} else {
+				break
+			}
+		}
+	} catch(error) {
+
+	}
+
+	if (correctTileCount === numberOfTilesToWin){
+		alert('Great job, you\'ve won')
+	}
+}
+
+
+checkWin();
