989713302f60d9d020a44ff9b74b45d74893418b
Korneliusz Chodnikiewicz <korneliuszchodnikiewicz@gmail.com>
2019-07-11 10:55:02 +0200

    Start work on pipe rotation function

--- app.py
+++ app.py
@@ -9,9 +9,9 @@ def load_main_page():
     return render_template('roboczy.html')
 
 
-# @app.route('/game')
-# def game():
-#     return render_template('game.html')
+@app.route('/game')
+def game():
+    return render_template('game.html')
 
 
 if __name__ == '__main__':
---
+++ static/rotate_pipe.js
@@ -0,0 +1,10 @@
+let straightElements = [
+	{"src": "static/icons/straight-vertical.png",
+	 "data-edge-1": "1",
+	 "data-edge-2": "3"},
+	{"src": "static/icons/straight-vertical.png",
+	 "data-edge-1": "4",
+	 "data-edge-2": "2"}
+];
+
+console.log(straightElements[1].[]);
